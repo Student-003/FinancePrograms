dfc0817133254c21025ec4e6deec7dbf4b9524d5
student-003 <creek.connor@yahoo.com>
2019-04-05 17:57:14 -0500

    added date validation

--- VaRCalculator/VaRCalculator.py
+++ VaRCalculator/VaRCalculator.py
@@ -28,13 +28,11 @@ from pprint import pprint
 # Import stock price data as CSV
 from alpha_vantage.timeseries import TimeSeries
 
-def plot_candles(df, title=None, volumeBars=False, colorFunction=None, overlays=None, technicals=None, technicalsTitles=None):
+def plot_candles(df, title=None, volumeBars=False, colorFunction=None, overlays=None):
     def default_color(index, open, close, low, high):
         return 'r' if open[index] > close[index] else 'g'
     colorFunction = colorFunction or default_color
     overlays = overlays or []
-    technicals = technicals or []
-    technicalsTitles = technicalsTitles or []
     open = df['Open']
     close = df['Close']
     low = df['Low']
@@ -45,8 +43,6 @@ def plot_candles(df, title=None, volumeBars=False, colorFunction=None, overlays=
     subplotCount = 1
     if volumeBars:
         subplotCount = 2
-    if technicals:
-        subplotCount += len(technicals)
 
     if subplotCount == 1:
         fig, ax1 = plt.subplots(1, 1)
@@ -62,12 +58,13 @@ def plot_candles(df, title=None, volumeBars=False, colorFunction=None, overlays=
     candles = ax1.bar(x, ocMax-ocMin, bottom=ocMin, color=candleColors, linewidth=0)
     lines = ax1.vlines(x, low, high, color=candleColors, linewidth=1)
     ax1.xaxis.grid(False)
-    ax1.xaxis.set_tick_params(which='major', length=3.0, direction='in', top=False)
+    ax1.xaxis.set_tick_params(which='major', length=1.5, direction='in', top=False)
     # Assume minute frequency if first two bars are in the same day.
     frequency = 'minute' if (df.index[1] - df.index[0]).days == 0 else 'day'
-    timeFormat = '%d-%m-%Y'
+    timeFormat = '%d-%m'
     if frequency == 'minute':
         timeFormat = '%H:%M'
+
     # Set X axis tick labels.
     plt.xticks(x, [date.strftime(timeFormat) for date in df.index], rotation='vertical')
     for overlay in overlays:
@@ -90,12 +87,6 @@ def plot_candles(df, title=None, volumeBars=False, colorFunction=None, overlays=
             volumeTitle = 'Volume (%s)' % volumeScale
         ax2.set_title(volumeTitle)
         ax2.xaxis.grid(False)
-    # Plot additional technical indicators
-    for (i, technical) in enumerate(technicals):
-        ax = subplots[i - len(technicals)] # Technical indicator plots are shown last
-        ax.plot(x, technical)
-        if i < len(technicalsTitles):
-            ax.set_title(technicalsTitles[i])
 
 
 def vizualize(tickerSymbol, df, candleDf, mean, sigma, tdf, tmean, tsigma):
@@ -103,11 +94,15 @@ def vizualize(tickerSymbol, df, candleDf, mean, sigma, tdf, tmean, tsigma):
     interactive(True)
 
     # Plot candle chart
+    #if (df.index[0] - df.index[-1]).days > 30
     candleTitle = tickerSymbol + " Price Chart"
-    plot_candles(candleDf, title=candleTitle, volumeBars=True)
-    #plt.figure(1)
-    #df['Close'].plot()
-    #plt.title(tickerSymbol + " Price Chart", weight='bold');
+
+    if (df.index[-1] - df.index[0]).days < 60:
+        plot_candles(candleDf, title=candleTitle, volumeBars=True)
+    else:
+        plt.figure(1)
+        df['Close'].plot()
+        plt.title(tickerSymbol + " Price Chart", weight='bold');
 
     # Plot daily returns
     plt.figure(2)
@@ -147,32 +142,119 @@ def pull_data(tickerSymbol):
     print('...Fetching data from Alpha Vantage API...')
     parser = configparser.ConfigParser()
     foundFile = parser.read(['./resources/secret.ini', './resources/template_secret.ini'])
-    apiKey = parser.get('API_KEYS','ALPHA_VANTAGE') if './secret.ini' in foundFile else parser.get('API_KEYS_TEMPLATE','ALPHA_VANTAGE')
+    apiKey = parser.get('API_KEYS','ALPHA_VANTAGE') if './resources/secret.ini' in foundFile else parser.get('API_KEYS_TEMPLATE','ALPHA_VANTAGE')
     if (apiKey == 'guest'):
-        print('\nNOTE: You are using the demo API Key. Please request an Alpha Vantage API key from https://www.alphavantage.co\n')
+        print('\n\nNOTE: You are using the demo API Key. Please request an Alpha Vantage API key from https://www.alphavantage.co\n\n')
     # Fetch price data
     # Alpha Vantage documentation and free API key can be found at https://www.alphavantage.co
     ts = TimeSeries(key=apiKey, output_format='pandas')
     data, meta_data = ts.get_daily(symbol=tickerSymbol, outputsize='full')
     outFile = "./resources/" + meta_data['2. Symbol'] + "PriceData.csv"
     data.to_csv(outFile)
-    print('\nInitial Data:')
+    print('\n\nInitial Data:')
     pprint(data.head())
     return data
+def checkInDataset(startYear, startMonth, startDay, endYear, endMonth, endDay):
+        errorMessage = '\nError: the {:s} of the start date you have entered is after the {:s} of the end date given\n\n'
+
+        # Check that the start date is before the end date
+        if (int(startYear) > int(endYear)):
+            print(errorMessage.format('year', 'year'))
+            exit()
+        elif (int(startYear) > int(endYear) and int(startMonth) > int(endMonth)):
+            print(errorMessage.format('month', 'month'))
+            exit()
+        elif (int(startYear) > int(endYear) and int(startMonth) > int(endMonth) and int(startDay) > int(endDay)):
+            print(errorMessage.format('day', 'day'))
+            exit()
+
+def validateDate (dirtyData, startDate, endDate):
+    daysShift = 0
+    start = startDate.split('-')
+    startYear, startMonth, startDay = start
+    end = endDate.split('-')
+    endYear, endMonth, endDay = end
+
+    # Check that the start date is before the end date
+    checkInDataset(startYear, startMonth, startDay, endYear, endMonth, endDay)
+    # Check that the endDate is in the dataframe
+
+
+    # Check that startDate is not on a weekend/holiday and startDate < endDate
+    while (startDate not in dirtyData.index):
+        checkInDataset(startYear, startMonth, startDay, endYear, endMonth, endDay)
+        # Increment the day value
+        startDay = int(startDay)
+        startMonth = int(startMonth)
+        startYear = int(startYear)
+        #print(startDay)
+        # Check if month has 31 day
+        if (startMonth in [1, 3, 5, 7, 8, 10, 12] and startDay < 31):
+            if (startDay < 9):
+                startDay += 1
+                startDay = "0" + str(startDay)
+            else:
+                startDay += 1
+                startDay = str(startDay)
+        elif(startMonth in [1, 3, 5, 7, 8, 10, 12] and startDay == 31):
+            if (startMonth == 10):
+                startMonth+=1
+                startMonth = str(startMonth)
+                startDay = "01"
+            elif(startMonth == 12):
+                startMonth = "01"
+                startDay = "01"
+                startYear += 1
+                startYear = str(startYear)
+            else:
+                startDay = "01"
+                startMonth += 1
+                startMonth = "0" + str(startMonth)
+        elif (startMonth in [4, 6, 9, 11] and startDay < 30):
+            if (startDay < 9):
+                startDay += 1
+                startDay = "0" + str(startDay)
+            else:
+                startDay += 1
+                startDay = str(startDay)
+        elif(startMonth in [4, 6, 9, 11] and startDay == 30):
+            if (startMonth == 11):
+                startMonth += 1
+                startMonth = str(startMonth)
+                startDay = "01"
+            else:
+                startMonth += 1
+                startMonth = "0" + str(startMonth)
+                startDay = "01"
+        # Check if the month is Febuary and if it is a leap year
+        elif(startMonth == 2):
+            if (startDay < 9):
+                startDay += 1
+                startDay = "0" + str(startDay)
+            elif (startDay < 28):
+                startDay += 1
+                startDay = str(startDay)
+            elif(startDay == 28):
+                if (((startYear % 2008) % 4 == 0) or ((2008 % startYear) % 4 == 0)):
+                    startDay += 1
+                    startDay = str(startDay)
+                else:
+                    startDay = "01"
+                    startMonth += 1
+                    startMonth = "0" + str(startMonth)
+            else:
+                startDay = "01"
+                startMonth += 1
+                startMonth = "0" + str(startMonth)
+        daysShift += 1
+        startDate = '{}-{}-{}'.format(startYear, startMonth, startDay)
+        #print(startDate)
+    return startDate, daysShift
 
 
 def clean_df(dirtyData, startDate, endDate):
-    daysShift = 0
-    startDateCopy = startDate
-    # Check that startDate is not on a weekend or holiday
-    while (startDate not in dirtyData.index):
-        # Increment the day value
-        #print(startDate[8:])
-        day = str(int(startDate[8:])+1)
-        daysShift += 1
-        startDate = ''.join((startDate[:8], day))
-        #print(startDate)
-
+    # Validate the given startDate and endDate
+    newStartDate, daysShift = validateDate(dirtyData, startDate, endDate)
     # Format column names
     print('\n...Cleaning data...\n')
     dirtyData.rename(columns={'1. open': 'Open', '2. high': 'High', '3. low': 'Low', '4. close': 'Close', '5. volume': 'Volume'}, inplace=True)
@@ -188,15 +270,15 @@ def clean_df(dirtyData, startDate, endDate):
     #pprint(df)
 
     # Trim data set to only include desired observations
-    startDateIdx = df.index.get_loc(startDate)
+    newStartDateIdx = df.index.get_loc(newStartDate)
     endDateIdx = df.index.get_loc(endDate)
-    df = df[startDateIdx:endDateIdx+1]
-    candlePlotData = dirtyData[startDateIdx:endDateIdx+1]
-    print('\nCleaned dataframe:\nChosen start date: ' + startDateCopy + '\nChosen end date: ' + endDate)
+    df = df[newStartDateIdx:endDateIdx+1]
+    candlePlotData = dirtyData[newStartDateIdx:endDateIdx+1]
+    print('\nCleaned dataframe,\nChosen start date: ' + startDate + '\nChosen end date: ' + endDate)
     if (daysShift>0):
-        print('\nError: there was an issue with the given start date and has been moved by ' + str(daysShift) + ' days\nThe start date is now: ' +  startDate)
+        print('\nError: there was an issue with the given start date and has been moved by ' + str(daysShift) + ' day(s)\n\nThe start date is now: ' +  newStartDate)
 
-    print('First observation:')
+    print('\nFirst observation:')
     pprint(df.head(1))
     print('\nLast observation:')
     pprint(df.tail(1))
@@ -254,14 +336,13 @@ def single_stock_VaR(tickerSymbol, startDate, endDate, vizData=False):
     print("\n")
 
 def portfolio_VaR():
-    startDate = '2014-04-05'
+    startDate = '2018-04-04'
     endDate = '2019-04-04'
     positions = pd.read_csv("./resources/portfolio.csv", delimiter=',')
     #print(positions)
 
     for index, row in positions.iterrows():
-        single_stock_VaR(row['Symbol'], startDate, endDate, vizData=False)
-
+        single_stock_VaR(row['Symbol'], startDate, endDate, vizData=True)
 
 
 if __name__ == '__main__':
@@ -269,4 +350,4 @@ if __name__ == '__main__':
     profile.run('portfolio_VaR()', 'resources/VaR_Stats')
     #profile.run("single_stock_VaR('AAPL', '2018-07-20', '2019-04-02', vizData=True)", "VaR_Stats")
     p = pstats.Stats('resources/VaR_Stats')
-    p.strip_dirs().sort_stats('time', 'ncalls').print_stats(1)
+    p.strip_dirs().sort_stats('time', 'ncalls').print_stats(0)
